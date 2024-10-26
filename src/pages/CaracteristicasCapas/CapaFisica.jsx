491b108111b63fa8d44585e7290c1cddf0aa4275
José Díaz <diazmarinjoseelias2007@gmail.com>
2024-10-25 19:57:19 -0500

    New section added

---
+++ src/pages/CaracteristicasCapas/CapaFisica.jsx
@@ -0,0 +1,5 @@
+import data from '../../data/CapaFisica.json'
+import ContentRenderer from '../../utils/ContentRenderer'
+export const CapaFisica = () => {
+	return <ContentRenderer data={data} />
+}
---
+++ src/pages/CaracteristicasCapas/CapaInternet.jsx
@@ -0,0 +1,5 @@
+import data from '../../data/CapaInternet.json'
+import ContentRenderer from '../../utils/ContentRenderer'
+export const CapaInternet = () => {
+	return <ContentRenderer data={data} />
+}
