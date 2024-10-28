f1c8083ee0fa21ab51a7cfec79f26f5ae73ed0ea
José Díaz <diazmarinjoseelias2007@gmail.com>
2024-10-28 16:07:31 -0500

    New information added

---
+++ src/pages/ArquitecturaRed/QuizArquitecturaRed.jsx
@@ -0,0 +1,5 @@
+import QuizRenderer from '../../utils/QuizRenderer'
+import data from '../../data/QuizArquitecturaRed.json'
+export const QuizArquitecturaRed = () => {
+	return <QuizRenderer quizData={data} />
+}
---
+++ src/pages/CaracteristicasCapas/QuizCaracteristicasCapas.jsx
@@ -0,0 +1,5 @@
+import QuizRenderer from '../../utils/QuizRenderer'
+import data from '../../data/QuizCaracteristicasCapas.json'
+export const QuizCaracteristicasCapas = () => {
+	return <QuizRenderer quizData={data} />
+}
